96b48d431f756fb49291bc926d1453666610e03f
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-10-14 19:27:16 +0530

    commit

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "Emerald",
+  theme: localStorage.getItem("streamify-theme") || "emerald",
   setTheme: (theme) => {
     localStorage.setItem("streamify-theme", theme);
     set({ theme });
