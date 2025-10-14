066d097426e490af19e1d9f49cfbaf2c750e596f
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2025-10-14 21:07:59 +0530

    commiit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -8,15 +8,9 @@ const Layout = ({ children, showSidebar = false }) => {
         {showSidebar && <Sidebar />}
 
         <div className="flex-1 flex flex-col">
-          {/* Added 'md:sticky md:top-0' to make navbar fixed on mobile but sticky on desktop */}
-          <div className="fixed top-0 left-0 right-0 z-30 md:static md:z-auto">
-            <Navbar />
-          </div>
-          
-          {/* Added padding to account for fixed navbar on mobile */}
-          <main className="flex-1 overflow-y-auto pt-16 md:pt-0 mt-16 md:mt-0">
-            {children}
-          </main>
+          <Navbar />
+
+          <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>
     </div>
